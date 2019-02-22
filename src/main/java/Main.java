151419ce80927d17ea3a1947156b3bd244b47295
Rafał Grzywacz <rafal.grzywacz@comarch.com>
2019-02-22 19:14:14 +0100

    Refactor EmailService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
-import email.EmailService;
+import email.EmailServiceImpl;
 import employee.Employee;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        EmailService emailService = new EmailService();
+        EmailServiceImpl emailServiceImpl = new EmailServiceImpl();
 
         Employee manager = new Employee("Janusz Kowalski");
 
         Employee workerOne = new Employee("Piotr Kozik");
         Employee workerTwo = new Employee("Adam Nowacki");
 
-        emailService.send(manager, Arrays.asList(workerOne,workerTwo),"titleOne","contentOne");
+        emailServiceImpl.send(manager, Arrays.asList(workerOne,workerTwo),"titleOne","contentOne");
 
 
 
--- src/main/java/email/EmailService.java
+++ src/main/java/email/EmailService.java
@@ -4,9 +4,6 @@ import employee.Employee;
 
 import java.util.List;
 
-public class EmailService {
-    public boolean send(Employee from, List<Employee> recipient, String title, String content) {
-        //TODO logic of sending an email.
-        return true;
-    }
+public interface EmailService {
+    boolean send(Employee from, List<Employee> recipient, String title, String content);
 }
